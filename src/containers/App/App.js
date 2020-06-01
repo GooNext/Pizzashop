a4889f2e744d9dce27ca2471bced0b0123c5ab90
Valery <52200731+GooNext@users.noreply.github.com>
2020-06-01 13:19:33 +0300

    csssuccess

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <Header/>
+      </div>
     </div>
   );
 }
